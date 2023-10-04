d63e353bd62a30df98e953f77e0af590b0569fab
Kien Ngo <trungkien08072000@gmail.com>
2023-10-04 11:31:30 +0700

    update

--- pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -1,12 +1,11 @@
 package com.udacity.pricing.domain.price;
 
+import lombok.Data;
+
 import java.math.BigDecimal;
 
-/**
- * Represents the price of a given vehicle, including currency.
- */
+@Data
 public class Price {
-
     private String currency;
     private BigDecimal price;
     private Long vehicleId;
@@ -17,30 +16,6 @@ public class Price {
     public Price(String currency, BigDecimal price, Long vehicleId) {
         this.currency = currency;
         this.price = price;
-        this.vehicleId = vehicleId;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public Long getVehicleId() {
-        return vehicleId;
-    }
-
-    public void setVehicleId(Long vehicleId) {
         this.vehicleId = vehicleId;
     }
 }
--- vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
@@ -1,6 +1,5 @@
 package com.udacity.vehicles.api;
 
-
 import com.udacity.vehicles.domain.car.Car;
 import com.udacity.vehicles.service.CarService;
 import org.springframework.hateoas.Resource;
@@ -14,12 +13,8 @@ import java.net.URISyntaxException;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 
-/**
- * Implements a REST-based controller for the Vehicles API.
- */
 @RestController
 @RequestMapping("/cars")
 class CarController {
@@ -32,50 +27,24 @@ class CarController {
         this.assembler = assembler;
     }
 
-    /**
-     * Creates a list to store any vehicles.
-     *
-     * @return list of vehicles
-     */
     @GetMapping
     Resources<Resource<Car>> list() {
         List<Resource<Car>> resources = carService.list().stream().map(assembler::toResource)
                 .collect(Collectors.toList());
-        return new Resources<>(resources,
-                linkTo(methodOn(CarController.class).list()).withSelfRel());
+        return new Resources<>(resources, linkTo(methodOn(CarController.class).list()).withSelfRel());
     }
 
-    /**
-     * Gets information of a specific car by ID.
-     *
-     * @param id the id number of the given vehicle
-     * @return all information for the requested vehicle
-     */
     @GetMapping("/{id}")
     Resource<Car> get(@PathVariable Long id) {
         return assembler.toResource(carService.findById(id));
     }
 
-    /**
-     * Posts information to create a new vehicle in the system.
-     *
-     * @param car A new vehicle to add to the system.
-     * @return response that the new vehicle was added to the system
-     * @throws URISyntaxException if the request contains invalid fields or syntax
-     */
     @PostMapping
     ResponseEntity<?> post(@Valid @RequestBody Car car) throws URISyntaxException {
         Resource<Car> resource = assembler.toResource(carService.save(car));
         return ResponseEntity.created(new URI(resource.getId().expand().getHref())).body(resource);
     }
 
-    /**
-     * Updates the information of a vehicle in the system.
-     *
-     * @param id  The ID number for which to update vehicle information.
-     * @param car The updated information about the related vehicle.
-     * @return response that the vehicle was updated in the system
-     */
     @PutMapping("/{id}")
     ResponseEntity<?> put(@PathVariable Long id, @Valid @RequestBody Car car) {
         car.setId(id);
@@ -83,12 +52,6 @@ class CarController {
         return ResponseEntity.ok(resource);
     }
 
-    /**
-     * Removes a vehicle from the system.
-     *
-     * @param id The ID number of the vehicle to remove.
-     * @return response that the related vehicle is no longer in the system
-     */
     @DeleteMapping("/{id}")
     ResponseEntity<?> delete(@PathVariable Long id) {
         carService.delete(id);
