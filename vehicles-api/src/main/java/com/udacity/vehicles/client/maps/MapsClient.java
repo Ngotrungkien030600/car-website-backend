c6d1e90cc77cfc4a54632d23d7a62e6b591311d3
Kien Ngo <trungkien08072000@gmail.com>
2023-10-04 11:22:16 +0700

    update MapsClient

--- vehicles-api/src/main/java/com/udacity/vehicles/client/maps/MapsClient.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/client/maps/MapsClient.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.Objects;
 
@@ -20,8 +21,7 @@ public class MapsClient {
     private final WebClient client;
     private final ModelMapper mapper;
 
-    public MapsClient(WebClient maps,
-                      ModelMapper mapper) {
+    public MapsClient(WebClient maps, ModelMapper mapper) {
         this.client = maps;
         this.mapper = mapper;
     }
@@ -30,29 +30,27 @@ public class MapsClient {
      * Gets an address from the Maps client, given latitude and longitude.
      *
      * @param location An object containing "lat" and "lon" of location
-     * @return An updated location including street, city, state and zip,
+     * @return A Mono of updated location including street, city, state and zip,
      * or an exception message noting the Maps service is down
      */
-    public Location getAddress(Location location) {
-        try {
-            Address address = client
-                    .get()
-                    .uri(uriBuilder -> uriBuilder
-                            .path("/maps")
-                            .queryParam("lat", location.getLat())
-                            .queryParam("lon", location.getLon())
-                            .build()
-                    )
-                    .retrieve()
-                    .bodyToMono(Address.class)
-                    .block();
-
-            mapper.map(Objects.requireNonNull(address), location);
-
-            return location;
-        } catch (Exception e) {
-            log.warn("Map service is down");
-            return location;
-        }
+    public Mono<Location> getAddress(Location location) {
+        return client
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/maps")
+                        .queryParam("lat", location.getLat())
+                        .queryParam("lon", location.getLon())
+                        .build()
+                )
+                .retrieve()
+                .bodyToMono(Address.class)
+                .onErrorResume(e -> {
+                    log.warn("Map service is down");
+                    return Mono.just(new Address()); // Return an empty address or handle the error as needed
+                })
+                .flatMap(address -> {
+                    mapper.map(Objects.requireNonNull(address), location);
+                    return Mono.just(location);
+                });
     }
 }
